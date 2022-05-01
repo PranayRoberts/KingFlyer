7994b1921aa51d2649bc87d4de3ea5040f00ca86
Harshasonu <96070138+Harshasonu@users.noreply.github.com>
2022-05-01 15:40:55 +0530

    Delete King-Flyer directory

--- King-Flyer/src/components/SideBar/SideBarData/General.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ConnectingAirportsRounded from "@mui/icons-material/ConnectingAirportsRounded";
-export const General = [
-  {
-    title: "Search Flights",
-    icon: <ConnectingAirportsRounded />,
-    link: "/",
-  },
-];
