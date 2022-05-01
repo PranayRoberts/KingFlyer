be290191361c3792ac55aee490bdf9c0ee1ead08
Harshasonu <96070138+Harshasonu@users.noreply.github.com>
2022-05-01 15:41:08 +0530

    Delete flight-ticket-booking-system directory

--- flight-ticket-booking-system/src/main/java/com/example/flightticketbookingsystem/flight/repository/FlightStatusRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.flightticketbookingsystem.flight.repository;
-
-import com.example.flightticketbookingsystem.flight.model.FlightStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FlightStatusRepo extends JpaRepository<FlightStatus,Integer> {
-}
